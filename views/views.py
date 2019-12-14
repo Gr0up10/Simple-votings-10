6d743949d5a2c8690804988d3c2614f3ba94af35
Горшенев Леонид <leon.leonc@yandex.ru>
2019-12-14 19:30:41 +0300

    minor changes

--- views/views.py
+++ views/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import *
 
+
 def index(request):
     context = {}
 
     context['votings'] = Voting.objects.all()
 
-
     return render(request, 'index.html', context)
+
