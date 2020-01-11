1f81797e4a0ef88842d0ac4f5ee8f21b56819c76
Васильев Андрей <rainbowshardofdeath@gmail.com>
2020-01-11 12:49:51 +0400

    Now user vote only one time. WORKS AT HOME PAGE ONLY

--- views/views.py
+++ views/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponse , redirect ,reverse
+from django.shortcuts import render, HttpResponse, redirect, reverse
 import datetime
 from .models import *
 from .forms import *
@@ -16,36 +16,44 @@ def index(request):
     for vote in context['votings']:
         li = vote.options()
         ids['v' + str(vote.id) + '_min'] = li[0].id
-        ids['v' + str(vote.id) + '_max'] = li[len(li)-1].id
+        ids['v' + str(vote.id) + '_max'] = li[len(li) - 1].id
     context['ids'] = ids
 
     if request.method == 'POST':
-        option_id = request.POST.getlist('answer')                           # getlist - функция , которая возвращяет list всех answer
-        for i in range(len(option_id)):                                      # добавление всех отвтов
-            if request.user.is_authenticated:
-                vote1 = Vote(option=Option.objects.get(id=option_id[i]), user=request.user)
-                vote1.save()
-            else:
-                return HttpResponse('Сперва войди!')
-
+        option_id = request.POST.getlist('answer')  # getlist - функция , которая возвращяет list всех answer
+        voting_id = request.POST.get('voting_id')
+        if not (Vote.objects.filter(user=request.user,
+                                    voting=Voting.objects.get(
+                                        id=voting_id)).exists()):  # Проверка голосовал ли пользователь в этом голосовании или нет
+            for i in range(len(option_id)):  # добавление всех ответов
+                if request.user.is_authenticated:
+                    vote1 = Vote(option=Option.objects.get(id=option_id[i]),
+                                 user=request.user,
+                                 voting=Voting.objects.get(id=voting_id))
+                    vote1.save()
+                else:
+                    return HttpResponse('Сперва войди!')
+        else:
+            return HttpResponse('Вы уже голосовали')  # красиво оформить вывод
     return render(request, 'index.html', context)
 
 
-def vote(request, option_id):  #    никак не используется !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def vote(request, option_id):  # никак не используется !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     context = {}
-    context['voting'] = Voting.objects.get(id = option_id)                  # добавление вопроса по его id
-    context['options'] = Option.objects.filter(voting_id = option_id)       # добавление всех его вариантов ответа
-    context['option_id'] = option_id                                        # номер вопроса
-    # обработка 
+    context['voting'] = Voting.objects.get(id=option_id)  # добавление вопроса по его id
+    context['options'] = Option.objects.filter(voting_id=option_id)  # добавление всех его вариантов ответа
+    context['option_id'] = option_id  # номер вопроса
+    # обработка
     if request.method == 'POST':
-        option_id = request.POST.getlist('answer')                           # getlist - функция , которая возвращяет list всех answer
-        for i in range(len(option_id)):                                      # добавление всех отвтов
+        option_id = request.POST.getlist('answer')  # getlist - функция , которая возвращяет list всех answer
+        for i in range(len(option_id)):  # добавление всех отвтов
             if request.user.is_authenticated:
                 vote1 = Vote(option=Option.objects.get(id=option_id[i]), user=request.user)
                 vote1.save()
             else:
                 return HttpResponse('Сперва войди!')
-    return render (request,'vote.html',context)
+    return render(request, 'vote.html', context)
+
 
 def create(request):
     context = {}
@@ -58,20 +66,19 @@ def create(request):
         if request.user.is_authenticated:
             main_text = request.POST.get('main_text')
             isCheckbox = bool(request.POST.get('isCheckbox'))
-            voting = Voting(question=main_text, author=request.user, isCheckbox = isCheckbox)
+            voting = Voting(question=main_text, author=request.user, isCheckbox=isCheckbox)
             voting.save()
 
             count = request.POST.get('count')
             if count:
-                for i in range(1, int(count)+1):
-                    text = request.POST.get('option'+str(i))
-                    option = Option(voting = voting, text =text)
+                for i in range(1, int(count) + 1):
+                    text = request.POST.get('option' + str(i))
+                    option = Option(voting=voting, text=text)
                     option.save()
-            tmp = Voting.objects.filter(author = request.user)    # отфильтровка вопросов данного пользователя
-            voting_id = tmp[len(tmp) - 1].id                      # определение id последнего вопроса
-            return redirect(vote,voting_id)                       # редирект на voting/voting_id
+            tmp = Voting.objects.filter(author=request.user)  # отфильтровка вопросов данного пользователя
+            voting_id = tmp[len(tmp) - 1].id  # определение id последнего вопроса
+            return redirect(vote, voting_id)  # редирект на voting/voting_id
         else:
             return HttpResponse('Сперва войди!')
 
     return render(request, 'creation.html', context)
-
